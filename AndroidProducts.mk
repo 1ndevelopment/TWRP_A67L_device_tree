d18ee93399395e339e3f126cebfb729019ad2361
1ndev <1ndevelopment@protonmail.com>
2026-05-23 14:18:29 -0700

    Add build configurations for the A67L device. This allows users to build different variants. It also specifies the device-specific product configuration.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/ofrp_A67L.mk
+
+COMMON_LUNCH_CHOICES := \
+    ofrp_A67L-eng \
+    ofrp_A67L-userdebug \
+    omni_A67L-userdebug \
+    fox_A67L-userdebug
--- ofrp_A67L.mk
+++ ofrp_A67L.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device.mk)
+$(call inherit-product, device/revoview/A67L/device.mk)
 
 PRODUCT_NAME := ofrp_A67L
 PRODUCT_DEVICE := A67L
