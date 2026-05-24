7cd55e4fb3b0b1d3777c0357a592a4cd0a0a4d82
1ndev <1ndevelopment@protonmail.com>
2026-05-24 07:55:30 -0700

    Remove PRODUCT_COPY_FILES. It conflicts with the build system. This is not needed for recovery.

--- ofrp_A67L.mk
+++ ofrp_A67L.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, device/revoview/A67L/device.mk)
 
+# Vendor PRODUCT_COPY_FILES conflict with the build system — not needed for recovery
+PRODUCT_COPY_FILES :=
+
 PRODUCT_NAME := ofrp_A67L
 PRODUCT_DEVICE := A67L
 PRODUCT_BRAND := FOXXD
