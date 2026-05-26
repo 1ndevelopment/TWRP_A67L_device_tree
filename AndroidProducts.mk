30c24ecb245f39263e54da0800d9d20242660796
1ndev <1ndevelopment@protonmail.com>
2026-05-26 09:40:49 -0700

    Rename recovery to OFRP. Updated product names and lunch choices to reflect the change.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,5 +2,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/ofrp_A67L.mk
 
 COMMON_LUNCH_CHOICES := \
-    ofrp_A67L-eng \
-    ofrp_A67L-userdebug
+    ofrp_A67L-ap2a-eng \
+    ofrp_A67L-ap2a-userdebug
--- ofrp_A67L.mk
+++ ofrp_A67L.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/revoview/A67L/device.mk)
 # Vendor PRODUCT_COPY_FILES conflict with the build system — not needed for recovery
 PRODUCT_COPY_FILES :=
 
-PRODUCT_NAME := twrp_A67L
+PRODUCT_NAME := ofrp_A67L
 PRODUCT_DEVICE := A67L
 PRODUCT_BRAND := FOXXD
 PRODUCT_MODEL := A67L
